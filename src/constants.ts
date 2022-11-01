c825e116b91eb3362a73c8a4d2532788ca1f112b
mbiet <mbietuko@udel.edu>
2022-11-01 17:30:45 -0400

    added interface

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,13 @@ export const ItemTypes = {
     PIC: "pic"
 };
 
+export type RoomType = "kitchen" | "bedroom" | "living room";
+
 export type Item = {
     position: [number, number];
     UID: number; // Unique ID
+    color: string;
+    height: number;
+    width: number;
+    image: string;
 };
